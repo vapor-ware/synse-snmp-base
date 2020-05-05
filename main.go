2d31ce96a4e11e8778d64a55fc0b15afba3529cb
Erick Daniszewski <edaniszewski@gmail.com>
2020-05-05 09:16:59 -0400

    ci: define binary main, required to release with goreleaser

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Printf(`
+The Synse SNMP plugin base is used as a common core for SNMP-base
+plugins. For more details on its implementation and usage, see:
+
+https://www.github.com/vapor-ware/synse-snmp-base
+`)
 }
