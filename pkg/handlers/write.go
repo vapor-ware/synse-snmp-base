277a3388bf5c0172e3025a13fe955d9dd92c733d
Erick Daniszewski <edaniszewski@gmail.com>
2020-05-05 09:11:42 -0400

    update: log and return error for writes - currently unimplemented

--- pkg/handlers/write.go
+++ pkg/handlers/write.go
@@ -1,9 +1,15 @@
 package handlers
 
-import "github.com/vapor-ware/synse-sdk/sdk"
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/vapor-ware/synse-sdk/sdk"
+)
 
 // writeHandlerFunc is the function which handles Writes for SNMP device handlers.
 func writeHandlerFunc(device *sdk.Device, data *sdk.WriteData) error {
 	// TODO: implement
-	return nil
+	log.Error("[snmp] SNMP writes are not yet implemented")
+	return fmt.Errorf("snmp writes not yet implmented in snmp plugin base")
 }
